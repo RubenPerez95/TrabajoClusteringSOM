23dc7d56e0870750d3bada72c299f5d8eee02970
Ruben Perez Rubio <ruben.perez7@alu.uclm.es>
2020-03-22 21:33:14 +0100

    Cálculo de grupos, de representes y valores importantes

--- CodigoSOM.R
+++ CodigoSOM.R
@@ -7,10 +7,11 @@ library(dplyr)
 
 
 ## Carga de datos del .CSV
-datosCSV <- read.csv('./Datos Muestra/Wholesale customers data.csv')
+datosOriginalesCSV <- read.csv('./Datos Muestra/Wholesale customers data.csv')
+
 
 ## Se eliminan las variables irrelevantes para el analisis. En este caso "Channel" y "Region"
-datosCSV <- select(datosCSV, -c(Channel, Region))
+datosCSV <- select(datosOriginalesCSV, -c(Channel, Region))
 
 ## Se comprueban los datos almacenados en datosCSV para ver si se han borrado las variables deseadas. Borrar cuando se haga correctamente la comprobacion.
 summary(datosCSV)
@@ -58,14 +59,15 @@ plot(mapaAuto2, type = "count")
 plot(mapaAuto2, shape = "straight")
 plot(mapaAuto2, type = "changes")
 
-## Podemos comprobar cuÃ¡ntos elementos existen en un nodo. QuizÃ¡ nos venga bien para el anÃ¡lisis.
+## Podemos comprobar cuantos elementos existen en un nodo. QuizÃ¡ nos venga bien para el anÃ¡lisis.
 elementosNodo <- table(mapaAuto2$unit.classif)
 print(elementosNodo)
 
 ## Podemos comprobar los nodos que se han asignado a cada registro del csv
 print(mapaAuto2$unit.classif) 
 
-## Se puede generar una grÃ¡fica de distancias. Representa la distancia euclidiana entre los vectores de cada neurona con su vecina.
+## Se puede generar una grafica de distancias. Representa la distancia euclidiana entre los vectores de cada neurona con su vecina.
+## Se obtienen los Outliers
 plot(mapaAuto2, type="dist.neighbours", shape = "straight")
 
 ## CLUSTERING CON SOM
@@ -78,7 +80,7 @@ agrupamiento <- hclust(distancia,method="ward.D2",members=elementosNodo)
 plot(agrupamiento,hang=-1,labels=F)
 
 ## Se prueba a hacer diversos grupos en base al resultado de la gráfica (se ve un buen resultado con 4 o 5 grupos)
-k <- 3
+k <- 4
 rect.hclust(agrupamiento,k)
 
 ## Se muestra a qué cluster pertenece cada grupo
@@ -90,7 +92,86 @@ plot(mapaAuto2,type="mapping",bgcol=c("paleturquoise","#F5D0FA","springgreen")[g
 add.cluster.boundaries(mapaAuto2,clustering=grupos)
 
 
+## CLUSTERING CON SOM sin Outliers
 
+
+## Carga de csv sin Outliers
+datosCSVSinOutliers <- read.csv('./Datos Muestra/Wholesale customers data Sin Outliers.csv')
+datosCSVSinOutliers <- select(datosCSVSinOutliers, -c(Channel, Region))
+summary(datosCSVSinOutliers)
+
+datosEscaladosOutliers <- scale(datosCSVSinOutliers)
+
+mapaAutoOutliers <- som(datosEscaladosOutliers, grid = somgrid(6,6,"hexagonal"), rlen = 1000)
+plot(mapaAutoOutliers, type = "count")
+plot(mapaAutoOutliers, shape = "straight")
+plot(mapaAutoOutliers, type = "changes")
+
+elementosNodoOutliers <- table(mapaAutoOutliers$unit.classif)
+
+## Realizar la matriz de distancia entre los nodos
+distanciaOutliers <- dist(getCodes(mapaAutoOutliers,1))
+
+## Se realiza el agrupamiento jerarquico con las distancias y los nodos y se dibuja el gráfico
+agrupamientoOutliers <- hclust(distanciaOutliers,method="ward.D2",members=elementosNodoOutliers)
+plot(agrupamientoOutliers,hang=-1,labels=F)
+
+## Se prueba a hacer diversos grupos en base al resultado de la gráfica (se ve un buen resultado con 4 o 5 grupos)
+k <- 1
+rect.hclust(agrupamientoOutliers,k)
+
+## Se muestra a qué cluster pertenece cada grupo
+gruposOutliers <- cutree(agrupamientoOutliers, k)
+print(gruposOutliers)
+
+## Se muestran los clusters en el grafico
+plot(mapaAutoOutliers,type="mapping",bgcol=c("paleturquoise","#F5D0FA","springgreen")[gruposOutliers],shape = "straight")
+add.cluster.boundaries(mapaAutoOutliers,clustering=gruposOutliers)
+
+## Cálculo de media para establecer el representante del grupo obtenido
+mediaRepresentante <- mean(arraySumaFilas)
+print(mediaRepresentante)
+
+### --- DATOS POR REGION --- ###
+
+## Filtrado de datos en funcion de las variables cualitativas de Region 
+datosLisboa <- select(filter(datosOriginalesCSV, Region == 1), -c(Channel, Region))
+datosOporto <- select(filter(datosOriginalesCSV, Region == 2), -c(Channel, Region))
+datosOtros <- select(filter(datosOriginalesCSV, Region == 3), -c(Channel, Region))
+
+## Ingresos totales por Region
+sumaIngresosLisboa <- sum(rowSums(datosLisboa, na.rm=FALSE, dims = 1))
+sumaIngresosOporto <- sum(rowSums(datosOporto, na.rm=FALSE, dims = 1))
+sumaIngresosOtros <- sum(rowSums(datosOtros, na.rm=FALSE, dims = 1))
+
+## Numero de clientes por Region
+clientesLisboa <- nrow(datosLisboa)
+clientesOporto <- nrow(datosOporto)
+clientesOtros <- nrow(datosOtros)
+
+## Ingresos medios por Region
+ingresosMediosLisboa <- sumaIngresosLisboa/clientesLisboa
+ingresosMediosOporto <- sumaIngresosOporto/clientesOporto
+ingresosMediosOtros <- sumaIngresosOtros/clientesOtros
+
+
+### --- DATOS POR CHANNEL --- ###
+
+## Filtrado de datos en funcion de las variables cualitativas de Channel
+datosMinorista <- select(filter(datosOriginalesCSV, Channel == 1), -c(Channel, Region))
+datosHosteleria <- select(filter(datosOriginalesCSV, Channel == 2), -c(Channel, Region))
+
+## Ingresos totales por Channel
+sumaIngresosMinorista <- sum(rowSums(datosMinorista, na.rm=FALSE, dims = 1))
+sumaIngresosHosteleria <- sum(rowSums(datosHosteleria, na.rm=FALSE, dims = 1))
+
+## Numero de clientes por Channel
+clientesMinorista <- nrow(datosMinorista)
+clientesHosteleria <- nrow(datosHosteleria)
+
+## Ingresos medios por Channel
+ingresosMediosMinorista <- sumaIngresosMinorista/clientesMinorista
+ingresosMediosHosteleria <- sumaIngresosHosteleria/clientesHosteleria
 
 
 
